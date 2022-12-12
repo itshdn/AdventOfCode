7792c9c40af065bf9e63a5367a3e9bf4f0f05409
itshdn <httymbt@gmail.com>
2022-12-12 14:07:10 -0800

    modified 2022 day12 solutions

--- 2022/day12/lib.py
+++ 2022/day12/lib.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 class Node:
     def __init__(self, val, row, col):
         self.value = val
@@ -16,9 +19,21 @@ class Node:
         if col < (len(grid[0]) - 1):
             self.edges.append(grid[row][col+1])
 
-    def get_valid_edges(self, visited):
+    def get_valid_edges_part1(self, visited):
+        res = []
+        for node in self.edges:
+            if node not in visited and node.value - self.value <= 1:
+                res.append(node)
+        return res
+
+    def get_valid_edges_part2(self, visited):
         res = []
         for node in self.edges:
             if node not in visited and node.value - self.value >= -1:
                 res.append(node)
         return res
+
+    def euc_dis(self, end):
+        x = abs(self.row - end.row)
+        y = abs(self.col - end.col)
+        return sqrt(x**2 + y**2)
--- 2022/day12/part2.py
+++ 2022/day12/part2.py
@@ -4,6 +4,7 @@ from heapq import heappush, heappop
 file = open("input.txt")
 
 grid = []
+S, E = 0, 0
 
 for line in file:
     grid.append(list(line.strip()))
@@ -23,21 +24,21 @@ for row_idx in range(len(grid)):
         grid[row_idx][col_idx].set_edges(grid, row_idx, col_idx)
 
 visited = set()
-queue = [[S.distance, S.euc_dis(E), (S.row, S.col)]]
+queue = [[E.distance, (E.row, E.col)]]
 
 while True:
-    this_row, this_col = heappop(queue)[2]
+    this_row, this_col = heappop(queue)[1]
     this_node: Node = grid[this_row][this_col]
 
-    if this_node == E:
-        print(E.distance)
+    if this_node.value == 1:
+        print(this_node.distance)
         break
     if this_node in visited:
         continue
     visited.add(this_node)
 
-    valid_edges = this_node.get_valid_edges_part1(visited)
+    valid_edges = this_node.get_valid_edges_part2(visited)
 
     for node in valid_edges:
         node.distance = this_node.distance + 1
-        heappush(queue, [node.distance, node.euc_dis(E), (node.row, node.col)])
+        heappush(queue, [node.distance, (node.row, node.col)])
